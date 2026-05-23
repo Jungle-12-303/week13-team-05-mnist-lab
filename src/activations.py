50591ca2cbaf82ff6baaad63cb184f2adae7457b
윤형민 <hyeongmin92@gmail.com>
2026-05-23 20:28:47 +0900

    feat: ReLU 순전파와 역전파 구현

--- src/activations.py
+++ src/activations.py
@@ -13,10 +13,11 @@ import numpy as np
 class ReLU:
     """
     ReLU(Rectified Linear Unit) 활성화 함수.
-
     은닉층에서 음수 값은 0으로 막고, 양수 값은 그대로 통과시킵니다.
     forward에서 만든 mask는 backward 때 "어느 위치로 gradient를 흘릴지" 결정하는 데 사용됩니다.
     """
+    def __init__(self):
+        self.mask = None
 
     def forward(self, x):
         """
@@ -26,8 +27,11 @@ class ReLU:
         Returns:
             x와 같은 shape. x > 0인 위치만 원래 값을 유지합니다.
         """
-        # TODO: x > 0 위치를 self.mask에 저장하고, 음수/0 위치는 0으로 바꾸세요.
-        raise NotImplementedError("ReLU.forward를 구현하세요.")
+        # x > 0 위치를 self.mask에 저장하고, 음수/0 위치는 0으로 바꾸세요.
+        self.mask = (x > 0)
+        out = np.zeros(x.shape)
+        out[self.mask] = x[self.mask]
+        return out
 
     def backward(self, dout):
         """
@@ -37,9 +41,11 @@ class ReLU:
         Returns:
             ReLU 입력 x에 대한 gradient. forward 때 x <= 0이었던 위치는 0입니다.
         """
-        # TODO: forward에서 저장한 self.mask를 이용해 gradient가 흐를 위치만 남기세요.
-        raise NotImplementedError("ReLU.backward를 구현하세요.")
-
+        # forward에서 저장한 self.mask를 이용해 gradient가 흐를 위치만 남기세요.
+        dx = np.zeros(dout.shape)
+        dx[self.mask] = dout[self.mask]
+        
+        return dx
 
 class Softmax:
     """
